4d75da1f443876c194aa57498d05992823b7c2dd
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2025-04-26 12:44:19 +0900

    Add new tool-specific models

--- chat.go
+++ chat.go
@@ -46,6 +46,9 @@ const (
 	GPT4ominiTranscribe       ChatGPTModel = "gpt-4o-mini-transcribe"
 	Whisper                   ChatGPTModel = "whisper-1"
 	// Tool-specific models
+	GPT4oSearchPreview        ChatGPTModel = "gpt-4o-search-preview"
+	GPT4oMiniSearchPreview    ChatGPTModel = "gpt-4o-mini-search-preview"
+	ComputerUsePreview        ChatGPTModel = "computer-use-preview"
 )
 
 type ChatGPTModelRole string
